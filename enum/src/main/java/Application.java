49d4b7e9606b819f1df41e7ec97d840228308d81
JinHui <jh731448520@163.com>
2020-02-12 23:20:43 +0800

    [JinHui]{feat}-{#2-6-3}:2.6.3Enum finish

--- enum/src/main/java/Application.java
+++ enum/src/main/java/Application.java
@@ -1,7 +1,22 @@
 public class Application {
 
   public static void main(String[] args) {
+    Student stu1 = new Student("Linda", "Female");
+    Student stu2 = new Student("Bob", "Male");
+    Student stu3 = new Student("Solider", "Male");
+    Student stu4 = new Student("Cindy", "Female");
 
+    Student[] stuList = {stu1, stu2, stu3, stu4};
+    Gender gender = Gender.FEMALE;
+
+    filterByGender(stuList, gender);
   }
 
+  public static void filterByGender(Student[] stuList, Gender gender) {
+    for (Student stu:stuList) {
+      if(stu.getGender().equals(gender.getGender())) {
+        System.out.println(stu.getName());
+      }
+    }
+  }
 }
---
+++ enum/src/main/java/Gender.java
@@ -0,0 +1,17 @@
+public enum Gender {
+    FEMALE("Female"), MALE("Male");
+
+    private String gender;
+
+    Gender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
