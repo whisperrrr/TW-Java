2102eb24777765d2f23e0e05a8a04d4f0e2abb7f
JinHui <jh731448520@163.com>
2020-02-14 21:23:55 +0800

    [JinHui]{fix}-{#2-5-3}: put function in teacher class

--- modifier/src/main/java/Application.java
+++ modifier/src/main/java/Application.java
@@ -7,30 +7,10 @@ public class Application {
       Teacher teacher1 = new Teacher("Solider", new Student[]{stu2});
       Teacher teacher2 = new Teacher("Bob" , new Student[]{stu1});
 
-      addScore(teacher1, 5);
-      addScore(teacher2,5);
-      printTeacher(teacher1);
-      printTeacher(teacher2);
-  }
-  //给学生加分
-  public static Teacher addScore(Teacher teacher, int score) {
-      Student[] studentList = teacher.getStudents();
-      for (Student stu: studentList) {
-          int scoreTemp = stu.getScore();
-          stu.setScore(scoreTemp + score);
-      }
-      teacher.setStudents(studentList);
-      return teacher;
-  }
-  //打印老师和其名下学生的信息
-  public static void printTeacher(Teacher teacher) {
-      System.out.println("教师姓名： " + teacher.getName());
-      Student[] studentList = teacher.getStudents();
-      for (Student stu: studentList) {
-          System.out.println("学生姓名： " + stu.getName());
-          System.out.println("学生成绩： " + stu.getScore());
-      }
-      System.out.println("==================");
-  }
+      teacher1.addScore(5);
+      teacher2.addScore(5);
 
+      teacher1.printTeacher();
+      teacher2.printTeacher();
+  }
 }
--- modifier/src/main/java/Teacher.java
+++ modifier/src/main/java/Teacher.java
@@ -18,4 +18,22 @@ public class Teacher {
     public void setStudents(Student[] students) {
         this.students = students;
     }
+
+    public void addScore(int score) {
+        Student[] studentList = getStudents();
+        for (Student stu: studentList) {
+            int scoreTemp = stu.getScore();
+            stu.setScore(scoreTemp + score);
+        }
+    }
+
+    public void printTeacher() {
+        System.out.println("教师姓名： " + getName());
+        Student[] studentList = getStudents();
+        for (Student stu: studentList) {
+            System.out.println("学生姓名： " + stu.getName());
+            System.out.println("学生成绩： " + stu.getScore());
+        }
+        System.out.println("==================");
+    }
 }
