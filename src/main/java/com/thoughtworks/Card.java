f11d848ed6541f49d7a73d0826aebab3f939d5f2
JinHui <jh731448520@163.com>
2020-02-14 19:51:37 +0800

    [JinHui]{feat}-{#2-7}:finish cardMachine

--- src/main/java/com/thoughtworks/Card.java
+++ src/main/java/com/thoughtworks/Card.java
@@ -3,12 +3,13 @@ package com.thoughtworks;
 import java.util.ArrayList;
 
 /*
-* 成员属性：花色（枚举），牌面（枚举），一副牌（static代码块）
+* 成员属性：花色（枚举），牌面（枚举），一副牌（static代码块）,一副牌的总张数
 * 成员方法：显示一张牌
 * */
 public class Card {
     private CardValue cardValue;
     private CardName cardName;
+    private static int cardNum;
     private static ArrayList<Card> cards = new ArrayList<Card>();
     //构造函数
     public Card() {
@@ -43,6 +44,14 @@ public class Card {
         Card.cards = cards;
     }
 
+    public static int getCardNum() {
+        return cardNum;
+    }
+
+    public static void setCardNum(int cardNum) {
+        Card.cardNum = cardNum;
+    }
+
     // 初始化一副牌
     static {
         for (CardName name:CardName.values()) {
@@ -50,6 +59,7 @@ public class Card {
                 cards.add(new Card(value, name));
             }
         }
+        cardNum = cards.size();
     }
 
     // 显示一张牌
--- src/main/java/com/thoughtworks/CardMachine.java
+++ src/main/java/com/thoughtworks/CardMachine.java
@@ -1,4 +1,64 @@
 package com.thoughtworks;
+/*
+* 自动抽牌机
+* */
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class CardMachine {
+    // 抽牌并显示
+    public static void drawCard() {
+        int num = getNum();
+        ArrayList<Card> cards = getCardsRandom(num);
+
+    }
+    // 获取正确输入
+    public static int getNum() {
+        while(true) {
+            System.out.println("请输入抽取的张数(小于等于52)：");
+
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+
+            if (num <= Card.getCardNum()) {
+                return num;
+            }
+
+            System.out.println("您的输入有误哦");
+        }
+    }
+    // 随机抽取n张牌并显示
+    public static ArrayList<Card> getCardsRandom(int n) {
+        ArrayList<Card> cards = Card.getCards();
+        ArrayList<Card> newCards = new ArrayList<>();
+        int[] randomNum = getRandomNum(n);
+
+        for (int num:randomNum) {
+            Card cardTemp = cards.get(num);
+            newCards.add(cardTemp);
+            System.out.println(cardTemp.showCard(cardTemp));
+        }
+
+        return newCards;
+    }
+    // 获取不重复随机数组
+    public static int[] getRandomNum(int n) {
+        int[] randomList = new int[n];
+        int numMax = Card.getCardNum();
+        Random randomNum = new Random();
+
+        for (int i = 0; i < n; ) {
+            int temp = randomNum.nextInt(numMax); //[0,52)
+            boolean flag = Arrays.asList(randomList).contains(temp);
+            if (!flag) {
+                randomList[i] = temp;
+                i += 1;
+            }
+        }
+
+        return randomList;
+    }
 }
