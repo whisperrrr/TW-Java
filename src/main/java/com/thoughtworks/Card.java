bf21f6a3b21f400ca064d226e0f7a2afa8466067
JinHui <jh731448520@163.com>
2020-02-14 17:20:54 +0800

    [JinHui]{feat}-{#2-7}:2.7 add basic class

---
+++ src/main/java/com/thoughtworks/Card.java
@@ -0,0 +1,7 @@
+package com.thoughtworks;
+/*
+* 成员属性：花色（枚举），牌面（枚举），一副牌（static代码块）
+* 成员方法：洗牌，显示一张牌
+* */
+public class Card {
+}
---
+++ src/main/java/com/thoughtworks/CardValue.java
@@ -0,0 +1,4 @@
+package com.thoughtworks;
+
+public enum CardValue {
+}
