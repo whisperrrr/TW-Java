72118038cb127167eb3682cb427489f0dbd290a2
JinHui <jh731448520@163.com>
2020-02-17 21:23:58 +0800

    [JinHui]{fix}-{#2-9huangMenJi}：add some introduction to APP.class

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -3,7 +3,11 @@ package com.thoughtworks;
 public class App {
 
   public static void main(String[] args) {
-    Restaurant restaurant = new Restaurant();
+    Restaurant restaurant = new Restaurant(" TW黄焖鸡 ");
+    System.out.println("Welcome to" + restaurant.getName());
+
+    System.out.println("我们的菜品有\n" + restaurant.showMenu());
+
     String result = restaurant.bestCharge("ITEM0001 x 1,ITEM0013 x 2,ITEM0022 x 1");
     System.out.println(result);
   }
--- src/main/java/com/thoughtworks/Restaurant.java
+++ src/main/java/com/thoughtworks/Restaurant.java
@@ -1,11 +1,42 @@
 package com.thoughtworks;
 
+import java.util.List;
+
+import static com.thoughtworks.DataProvider.getDishes;
+
 public class Restaurant {
   private String name;
+
+  public Restaurant() {
+  }
+
+  public Restaurant(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 
   public static String bestCharge(String selectedItems) {
     Order order = new Order(selectedItems);
     return String.valueOf(order.renderOrder());
   }
 
+  public static String showMenu() {
+    List<Dish> dishes = getDishes();
+    StringBuilder Menu = new StringBuilder();
+
+    for (Dish dish:dishes) {
+      Menu.append("ID: ").append(dish.getId()).append(" ");
+      Menu.append("商品: ").append(dish.getName()).append(" ");
+      Menu.append("价格: ").append(dish.getPrice()).append('\n');
+    }
+
+    return String.valueOf(Menu);
+  }
 }
