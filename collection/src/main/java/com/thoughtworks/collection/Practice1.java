bac1ea1461a5ba764c08ea4afb1deaaa78117dbe
JinHui <jh731448520@163.com>
2020-02-19 19:12:37 +0800

    [JinHui]{feat}-{#3-1}:finish task

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.collection;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -11,6 +13,12 @@ public class Practice1 {
      *  生成10个0到20的随机整数，要求不能重复
      */
     public static Collection<Integer> randomNumber() {
-        return null;
+        Random random = new Random();
+        HashSet<Integer> set = new HashSet<>();
+
+        while (set.size() < 10) {
+            set.add(random.nextInt(21));
+        }
+        return set;
     }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,18 +1,47 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
     public static void main(String[] args) {
         //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+        List<Integer> list = getNumbers();
+        //1.for循环遍历打印
+        for (int i = 0; i < list.size(); i++) {
+            int temp = list.get(i);
+            System.out.println(temp);
+        }
+        System.out.println("================================");
+
+        //2.增强for循环遍历打印
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+        System.out.println("================================");
+
+        //3.迭代器遍历打印
+        Iterator<Integer> iterator = list.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
     }
 
     /**
      * 创建一个List 然后在List中添加0到100整数
      */
     public static List<Integer> getNumbers() {
-        return null;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+
+        return list;
     }
 
 
