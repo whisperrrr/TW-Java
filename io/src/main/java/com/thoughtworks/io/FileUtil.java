a6a2eafd1939f4cec9e1bf67d2e0531b76e41d7a
JinHui <jh731448520@163.com>
2020-02-24 16:41:57 +0800

    [JinHui]{feat}-{#3-4}:3.4IO finish

--- io/src/main/java/com/thoughtworks/io/FileUtil.java
+++ io/src/main/java/com/thoughtworks/io/FileUtil.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileUtil {
@@ -13,6 +17,47 @@ public class FileUtil {
      * 例如把a文件夹(a文件夹下有1.txt和一个空文件夹c)复制到b文件夹，复制完成以后b文件夹下也有一个1.txt和空文件夹c
      */
     public static void copyDirectory(File from, File to) throws IOException {
-
+        if (!to.exists()) {
+            to.mkdirs();
+        } else {
+            delete(to);
+        }
+        File[] listFiles = from.listFiles();
+        for (File file:listFiles) {
+            if (file.isFile()) {
+                copyFile(new File(from + "\\" + file.getName()),new File(to + "\\" + file.getName()));
+            } else {
+                copyDirectory(new File(from + "\\" + file.getName()),new File(to + "\\" + file.getName()));
+            }
+        }
+    }
+    //清空文件夹。保证to文件夹在复制之前为空
+    private static void delete(File to) {
+        if (to.isFile()) {
+            to.delete();
+        } else {
+            for (File file:to.listFiles()) {
+                delete(file);
+            }
+        }
+    }
+    //复制文件
+    public static void copyFile(File fromFile, File toFile){
+        BufferedInputStream inputStream = null;
+        BufferedOutputStream outputStream = null;
+        try {
+            inputStream = new BufferedInputStream(new FileInputStream(fromFile));
+            outputStream = new BufferedOutputStream(new FileOutputStream(toFile));
+            byte[] buffer = new byte[1024];
+            int n = 0;
+            while ((n = inputStream.read(buffer))!= -1) {
+                outputStream.write(buffer,0, n);
+            }
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
